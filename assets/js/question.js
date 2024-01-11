cc76bd1ed0fc26ec1954214e23589ad6f87f2413
Gary Sorohan <148573610+Soro82@users.noreply.github.com>
2024-01-11 16:17:38 +0000

    add questions array to questions.js

---
+++ assets/js/question.js
@@ -0,0 +1,17 @@
+const questions = [
+    {
+        question: "How many points does a forward get for scoring a goal?",
+        answers: [4,5,6],
+        correctAnswer: 4
+    },
+    {
+        question: "How many points does a midfielder get for scoring a goal?",
+        answers: [3,4,5],
+        correctAnswer: 5
+    },
+    {
+        question: "How many points does a defender get for scoring a goal?",
+        answers: [4,5,6],
+        correctAnswer: 5
+    },
+]

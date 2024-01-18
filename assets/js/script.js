84065d5bbe31c691ffbc85dc7c747233598ba381
Gary Sorohan <148573610+Soro82@users.noreply.github.com>
2024-01-18 12:59:57 +0000

    remove checkAnswerClicked function

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,6 +1,6 @@
 
-document.getElementById('nextQ').addEventListener('click', nextQuestion);
-document.getElementById('start').addEventListener('click', checkName);
+
+
 const displayAnswers = document.getElementsByClassName('answers');
 const questionArea = document.getElementsByClassName('question-area')[0];
 const welcomeArea = document.getElementsByClassName('welcome-area')[0];
@@ -8,10 +8,26 @@ const welcomeArea = document.getElementsByClassName('welcome-area')[0];
 let numArray = [];
 let questionNumDisplay = 0;
 let currentScore = 0;
+let questionNum = 0;
+let num;
+let answerBoxPicked = 0;
 
 
 document.addEventListener('DOMContentLoaded', function() {
+
+    let answers = document.getElementsByClassName('answers');
+
+    for (let answer of answers) {
+        answer.addEventListener('click', function() {
+            answerBoxPicked = answer;
+            console.log(answerBoxPicked);
+        });       
+       
+    }
     questionArea.style.display = 'none';
+    document.getElementById('start').addEventListener('click', checkName);
+    document.getElementById('nextQ').addEventListener('click', nextQuestion);
+    
 })
 
 function checkName() {
@@ -19,7 +35,7 @@ function checkName() {
     let warning = document.getElementById('warning-message');
     if  (name === '') {
         warning.innerText = 'You have not entered a name.';
-        start.addEventListener('click', checkName);
+        checkName();
     } else {
         // document.getElementById('name').innerText = name;
         // document.getElementById('question-num').innerText = 1;
@@ -52,7 +68,7 @@ function pickQuestion() {
     }
     
     document.getElementById('question-num').innerText = questionNumDisplay;
-    console.log(questionNumDisplay);
+    // console.log(questionNumDisplay);
 }
 
 /**
@@ -62,46 +78,22 @@ function setAnswers(num) {
     displayAnswers[0].innerText = questions[num].answers[0];
     displayAnswers[1].innerText = questions[num].answers[1];
     displayAnswers[2].innerText = questions[num].answers[2];
-    checkAnswerClicked(num);
+    checkAnswer(num);
 }
 
-/**
- * Checks which answer the user clicked on and sends it to the checkAnswer function.
- */
-function checkAnswerClicked(questionNum) {
-    let answerBox1 = document.getElementById('answer1');
-    let answerBox2 = document.getElementById('answer2');
-    let answerBox3 = document.getElementById('answer3');
-    let answerClicked = 0;
-    
-    answerBox1.addEventListener('click', function(){
-        answerClicked = parseInt(document.getElementById('answer1').innerText);
-        checkAnswer(questionNum, answerClicked, 1); 
-    })
-
-    answerBox2.addEventListener('click', function(){
-        answerClicked = parseInt(document.getElementById('answer2').innerText);
-        checkAnswer(questionNum, answerClicked, 2);
-    })
-
-    answerBox3.addEventListener('click', function(){
-        answerClicked = parseInt(document.getElementById('answer3').innerText);
-        checkAnswer(questionNum, answerClicked, 3);
-    })   
-    
-}
-
-function checkAnswer(questionNumber, answerPicked, answerBoxNum) {
-    let correctAns = questions[questionNumber].correctAnswer;
+function checkAnswer(questionNum) {
+    let correctAns = questions[questionNum].correctAnswer;
+    let boxPicked = answerBoxPicked + 1;
+    let answerPicked = parseInt(document.getElementById(`answer${boxPicked}`).innerText);;
     
     if (correctAns === answerPicked) {
         document.getElementById('question-response').innerText = "Correct answer. Well done.";
-        document.getElementById(`answer${answerBoxNum}`).style.backgroundColor = 'green';
+        document.getElementById(`answer${boxPicked}`).style.backgroundColor = 'green';
         currentScore++;
         document.getElementById('correct-answers').innerText = currentScore;
     } else {
         document.getElementById('question-response').innerText = "Incorrect answer. Better luck next time.";
-        document.getElementById(`answer${answerBoxNum}`).style.backgroundColor = 'red';
+        document.getElementById(`answer${boxPicked}`).style.backgroundColor = 'red';
     }
     nextQuestion();
 }

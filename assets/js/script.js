cc173a60320101ae6f85eaeb6ace795165a07704
Gary Sorohan <148573610+Soro82@users.noreply.github.com>
2024-01-18 18:02:27 +0000

    fix checkAnswer function

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,50 +1,41 @@
 
-
-
 const displayAnswers = document.getElementsByClassName('answers');
 const questionArea = document.getElementsByClassName('question-area')[0];
 const welcomeArea = document.getElementsByClassName('welcome-area')[0];
-
+const start = document.getElementById('start');
 let numArray = [];
 let questionNumDisplay = 0;
 let currentScore = 0;
-let questionNum = 0;
-let num;
 
-    
 
 document.addEventListener('DOMContentLoaded', function() {
-
-    // questionArea.style.display = 'none';
-    // document.getElementById('start').addEventListener('click', pickQuestion);
-    document.getElementById('nextQ').addEventListener('click', nextQuestion);
-    welcomeArea.style.display = 'none';
- 
+    questionArea.style.display = 'none';
+    start.addEventListener('click', checkName);
 })
 
-// function checkName() {
-//     let name = document.getElementById('name').value;
-//     let warning = document.getElementById('warning-message');
-//     if  (name === '') {
-//         warning.innerText = 'You have not entered a name.';
-//         checkName();
-//     } else {
-//         // document.getElementById('name').innerText = name;
-//         // document.getElementById('question-num').innerText = 1;
-//         questionArea.style.display = 'block';
-//         welcomeArea.style.display = 'none';
-//         pickQuestion();
-//     }
-// }
+function checkName() {
+    let name = document.getElementById('name').value;
+    let warning = document.getElementById('warning-message');
+    if  (name === '') {
+        warning.innerText = 'You have not entered a name.';
+        start.addEventListener('click', checkName);
+    } else {
+        // document.getElementById('name').innerText = name;
+        // document.getElementById('question-num').innerText = 1;
+        questionArea.style.display = 'block';
+        welcomeArea.style.display = 'none';
+        pickQuestion();
+    }
+}
 
 /**
  * Picks a random question from the questions array and checks to see if it was asked previously
  */
 function pickQuestion() {
-    questionArea.style.display = 'block';
-    welcomeArea.style.display = 'none';
     // generate random number between 0 and 7
-    num = Math.floor(Math.random() * 8);
+    let num = Math.floor(Math.random() * 8);
+    console.log(num);
+    console.log(numArray);
     
     if (numArray.includes(num)) {
         pickQuestion();
@@ -62,7 +53,7 @@ function pickQuestion() {
     }
     
     document.getElementById('question-num').innerText = questionNumDisplay;
-    // console.log(questionNumDisplay);
+    
 }
 
 /**
@@ -72,38 +63,32 @@ function setAnswers(num) {
     displayAnswers[0].innerText = questions[num].answers[0];
     displayAnswers[1].innerText = questions[num].answers[1];
     displayAnswers[2].innerText = questions[num].answers[2];
-    // checkAnswerClicked(num);
-    document.addEventListener('click', checkAnswerClicked);
+    checkAnswerClicked(num);
 }
 
 /**
  * Checks which answer the user clicked on and sends it to the checkAnswer function.
  */
-function checkAnswerClicked(e) {
-    let questionNum = num;
+function checkAnswerClicked(questionNum) {
     let answerBox1 = document.getElementById('answer1');
     let answerBox2 = document.getElementById('answer2');
     let answerBox3 = document.getElementById('answer3');
-    let answerClicked = parseInt(e.target.innerText);
-    let boxNum = e.target.dataset;
-    console.log(boxNum);
-    console.log(answerClicked);
+    let answerClicked = 0;
     
-    // answerBox1.addEventListener('click', function(){
-    //     answerClicked = parseInt(answerBox1.innerText);
-    //     boxNum = 1; 
-    // })
+    answerBox1.addEventListener('click', function(){
+        answerClicked = parseInt(document.getElementById('answer1').innerText);
+        checkAnswer(questionNum, answerClicked, 1); 
+    })
 
-    // answerBox2.addEventListener('click', function(){
-    //     answerClicked = parseInt(answerBox2.innerText);
-    //     boxNum = 2;
-    // })
+    answerBox2.addEventListener('click', function(){
+        answerClicked = parseInt(document.getElementById('answer2').innerText);
+        checkAnswer(questionNum, answerClicked, 2);
+    })
 
-    // answerBox3.addEventListener('click', function(){
-    //     answerClicked = parseInt(answerBox3.innerText);
-    //     boxNum = 3;
-    // })   
-    checkAnswer(questionNum, answerClicked, boxNum);
+    answerBox3.addEventListener('click', function(){
+        answerClicked = parseInt(document.getElementById('answer3').innerText);
+        checkAnswer(questionNum, answerClicked, 3);
+    })   
     
 }
 
@@ -119,16 +104,18 @@ function checkAnswer(questionNumber, answerPicked, answerBoxNum) {
         document.getElementById('question-response').innerText = "Incorrect answer. Better luck next time.";
         document.getElementById(`answer${answerBoxNum}`).style.backgroundColor = 'red';
     }
-
+    nextQuestion();
 }
 
 function nextQuestion() {
-    
-    document.getElementById('answer1').style.backgroundColor = 'plum';
-    document.getElementById('answer2').style.backgroundColor = 'plum';
-    document.getElementById('answer3').style.backgroundColor = 'plum';
-    document.getElementById('question-response').innerText = "";
-    pickQuestion();
+    let nextQuestion = document.getElementById('nextQ');
+    nextQuestion.addEventListener('click', function(){
+        pickQuestion();
+        document.getElementById('answer1').style.backgroundColor = 'plum';
+        document.getElementById('answer2').style.backgroundColor = 'plum';
+        document.getElementById('answer3').style.backgroundColor = 'plum';
+        document.getElementById('question-response').innerText = "";
+    })
 }
 
 function endQuiz() {

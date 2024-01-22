63186bd5d5f550f36bc79d1ddd993f9ab4ee68dd
Gary Sorohan <148573610+Soro82@users.noreply.github.com>
2024-01-22 15:00:01 +0000

    add hard questions to question file

--- assets/js/question.js
+++ assets/js/question.js
@@ -40,3 +40,89 @@ const questions = [
         correctAnswer: -2
     },
 ]
+
+const easyQuestions = [
+    {
+        question: "How many points does a player get for playing 60 minutes or more?",
+        answers: [1,2,3],
+        correctAnswer: 2
+    },
+    {
+        question: "How many points does a goalkeeper get for every 3 shots saved?",
+        answers: [1,2,3],
+        correctAnswer: 1
+    },
+    {
+        question: "How many points does a defender get for scoring a goal?",
+        answers: [4,5,6],
+        correctAnswer: 6
+    },
+    {
+        question: "How many points does a player get for assisting a goal?",
+        answers: [3,4,5],
+        correctAnswer: 3
+    },
+    {
+        question: "How many points does a defender get for a clean sheet?",
+        answers: [3,4,5],
+        correctAnswer: 4
+    },
+    {
+        question: "How many points does a goalkeeper get for a clean sheet?",
+        answers: [3,4,5],
+        correctAnswer: 4
+    },
+    {
+        question: "How many points does a forward get for scoring a goal?",
+        answers: [4,5,6],
+        correctAnswer: 4
+    }, 
+    {
+        question: "How many points does a player get for a yellow card?",
+        answers: [-1,-2,-3],
+        correctAnswer: -1
+    },
+]
+
+const hardQuestions = [
+    {
+        question: "How many points does a goalkeeper get for a penalty save?",
+        answers: [3,4,5],
+        correctAnswer: 5
+    },  
+    {
+        question: "How many points does a player get for a red card?",
+        answers: [-1,-2,-3],
+        correctAnswer: -3
+    },
+    {
+        question: "How many points does a player get for each own goal?",
+        answers: [-1,-2,-3],
+        correctAnswer: -2
+    },
+    {
+        question: "How many points does a player get for missing a penalty?",
+        answers: [-1,-2,-3],
+        correctAnswer: -2
+    },
+    {
+        question: "How many points does a goalkeeper get for every 2 goals conceded?",
+        answers: [-1,-2,-3],
+        correctAnswer: -1
+    },
+    {
+        question: "How many points does a defender get for every 2 goals conceded?",
+        answers: [-1,-2,-3],
+        correctAnswer: -1
+    },
+    {
+        question: "How many points does a goalkeeper get for scoring a goal?",
+        answers: [4,5,6],
+        correctAnswer: 6
+    },
+    {
+        question: "How many points does a midfielder get for scoring a goal?",
+        answers: [3,4,5],
+        correctAnswer: 5
+    },
+]
